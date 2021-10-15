a3467dc4fdd8bf0896bfc42956cd1a8547e1a1df
Jangchun Lee <ginameeee@mgail.com>
2021-10-15 18:01:44 +0900

    CRA로 앱 구성

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fe-test-env
+
 storybook, jest, cypress 로 구성되는 테스트환경을 구성해본다.
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  console.log(process.env.NODE_ENV);
+
+  return (
+    <div className="App">
+      <h1>Hello React!</h1>
+    </div>
+  );
+}
+
+export default App;
