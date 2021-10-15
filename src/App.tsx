3a54772d2acdf606888871ab3bc962aaeea487f0
Jangchun Lee <ginameeee@mgail.com>
2021-10-15 18:27:10 +0900

    react-router(-dom) 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import About from "./components/pages/About";
+import Dashboard from "./components/pages/Dashboard";
+import Home from "./components/pages/Home";
 
 function App() {
   console.log(process.env.NODE_ENV);
 
   return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
